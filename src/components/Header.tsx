678fa827034ffe681ffe404b7caa4c93f5f6c669
bogdanilchenko1987 <bogdan_ilchenko@ukr.net>
2025-02-24 14:45:44 +0200

    clean up code and finish

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,3 @@
-// interface HeaderProps {
-//   onCartClick: () => void;
-//   cartCount: number;
-// }
-
-// export default function Header({ onCartClick, cartCount }: HeaderProps) {
-//   return (
-//     <header className="header">
-//       <p className="header__title">My Store</p>
-//       <nav className="header__nav">
-//         <p onClick={onCartClick} className=" header__nav__text">
-//           Cart
-//           {cartCount > 0 && (
-//             <span className="header__nav__count">{cartCount}</span>
-//           )}
-//         </p>
-//       </nav>
-//     </header>
-//   );
-// }
-
 import { useState } from 'react';
 import Modal from './Modal';
 import text from '../text.json';
